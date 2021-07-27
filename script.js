7d129cc217638ec2ca85866e8cccc097e8270ab9
Prasoon Shakya <pras95shakya@gmail.com>
2021-07-27 16:01:13 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+function onClickHandler() {
+    let name1= document.getElementById("nameInput1").value;
+    let name2= document.getElementById("nameInput2").value;
+    document.getElementById("wynisco").textContent = name1 + " " + name2;
+}
